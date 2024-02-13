afd0a5c1a4b45a9bf9d8726bfd3ac2a680635393
Caleb Rogers <caleb@calebjay.com>
2024-02-13 14:51:20 +0800

    Actually use sveltekit huge example app lol

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Counter from './Counter.svelte';
+	import welcome from '$lib/images/svelte-welcome.webp';
+	import welcome_fallback from '$lib/images/svelte-welcome.png';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>
+
+	</h1>
+
+
+</section>
+
+<style>
+</style>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,28 @@
+<script>
+</script>
+
+<header>
+  <h1>
+    Calorie Rolling Average Calculator
+  </h1>
+  <small>
+    by <a href="https://508.dev">508.dev</a>
+  </small>
+</header>
+
+<style>
+header {
+    margin-top: 10px;
+    display: flex;
+    justify-content: space-between;
+    align-items: baseline;
+    width: 100%;
+    height: 40px;
+}
+small {
+    vertical-align: center;
+}
+h1 {
+    font-size: 1.2rem;
+}
+</style>
