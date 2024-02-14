6f595d549cf0edad9875e2b69be9ebeef061f446
Caleb Rogers <caleb@calebjay.com>
2024-02-14 21:52:12 +0800

    Store persists to localStorage

--- src/stores.ts
+++ src/stores.ts
@@ -1,9 +1,26 @@
-import { writable, derived } from 'svelte/store';
+import { writable, get } from 'svelte/store';
 
 export type CalorieDateMap = {
 	[key: string]: number | null;
 };
 
-export const calorieDateMap = writable({
-	// [new Date().toDateString()]: 0,
-});
+function createCalorieDateMap() {
+	const store = writable({} as CalorieDateMap);
+	const { subscribe, set } = store;
+
+	localStorage.calorieMap && set(JSON.parse(localStorage.calorieMap));
+
+	return {
+		subscribe,
+		set: (val: CalorieDateMap) => {
+			localStorage.calorieMap = JSON.stringify(val);
+		},
+		update: (cb: (arg0: CalorieDateMap) => CalorieDateMap) => {
+			const newStore = cb(get(store));
+			localStorage.calorieMap = JSON.stringify(newStore);
+			set(newStore);
+		}
+	};
+}
+
+export const calorieDateMap = createCalorieDateMap();
